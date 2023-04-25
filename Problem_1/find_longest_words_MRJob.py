058baf9e7c98d4b726737829747d818843d9b84d
Reese <Nathanael.G-CTR.Reese@faa.gov>
2023-04-25 14:16:57 -0500

    uhh does this work?

--- Problem_1/find_longest_words_MRJob.py
+++ Problem_1/find_longest_words_MRJob.py
@@ -1,16 +1,23 @@
 from mrjob.job import MRJob
 
 
-class MRWordFrequencyCount(MRJob):
+class MRLongestWord(MRJob):
 
     def mapper(self, _, line):
-        yield "chars", len(line)
-        yield "words", len(line.split())
-        yield "lines", 1
+        words = line.split()
+        for word in words:
+            yield word[0], word
 
-    def reducer(self, key, values):
-        yield key, sum(values)
-
+    def reducer(self, initial_char, words):
+        longest_word = ''
+        longest_words = []
+        for word in words:
+            if len(word) > len(longest_word):
+                longest_word = word
+                longest_words = [word]
+            elif len(word) == len(longest_word):
+                longest_words.append(word)
+        yield initial_char, longest_words
 
 if __name__ == '__main__':
-    MRWordFrequencyCount.run()
+    MRLongestWord.run()
